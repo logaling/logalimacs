9ef413b2beae19a0a2fb04c76c7f1dc110698bc6
yuta yamada <yamada@clear-code.com>
2011-12-27 10:11:16 +0900

    apply GPL3

--- logalimacs.el
+++ logalimacs.el
@@ -1,4 +1,19 @@
-;;;*this is wrapped program for logaling-command
+;;; this is wrapped program for logaling-command
+;;;
+;;; Copyright (C) 2011  yuta yamada <yamada@clear-code.com>
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (defun loga-logaling-prompt-command (cmd arg &optional nobuffer)
   "yet can't be docstring"
