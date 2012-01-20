e617de4bb63c08b8e47361c95770db5811655d3f
yuta yamada <yamada@clear-code.com>
2012-01-20 13:36:37 +0900

    apply command for flymake err output

--- logalimacs.el
+++ logalimacs.el
@@ -177,5 +177,25 @@
   (setq loga-fly-mode nil)
   (message "loga-fly-mode disable"))
 
+;;;###autoload
+(defun loga-get-flymake-error ()
+  (interactive)
+  (let* ((line-no            (flymake-current-line-no))
+         (line-err-info-list (nth 0 (flymake-find-err-info flymake-err-info
+                                                           line-no)))
+         (count              (length line-err-info-list)))
+    (while (> count 0)
+      (when line-err-info-list
+        (let* ((file       (flymake-ler-file (nth (1- count)
+                                                  line-err-info-list)))
+               (full-file (flymake-ler-full-file (nth (1- count)
+                                                      line-err-info-list)))
+               (text      (flymake-ler-text (nth (1- count)
+                                                 line-err-info-list)))
+               (line      (flymake-ler-line (nth (1- count)
+                                                 line-err-info-list))))
+          (loga-make-buffer (format "[%s] %s" line text))))
+      (setq count (1- count)))))
+
 (provide 'logalimacs)
 ;;; logalimacs.el ends here
