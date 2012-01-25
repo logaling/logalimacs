ce16a2e2c1cb9ee9155c568750f5c623b5daefd4
yuta yamada <yamada@clear-code.com>
2012-01-26 01:04:01 +0900

    delete newline

--- logalimacs.el
+++ logalimacs.el
@@ -21,7 +21,6 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
 ;; This is Front-end to logaling-command for Ruby gems.
 ;; Logalimacs.el lookup to registered term at logaling-command and,
 ;; Executes other commands for logaling-command from emacs.
