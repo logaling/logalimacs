ec65156dad10941947440a7468fbda18dd0ba47a
yuta yamada <yamada@clear-code.com>
2012-06-07 13:44:58 +0900

    fix a typo (Licence -> License)

--- logalimacs.el
+++ logalimacs.el
@@ -7,7 +7,7 @@
 ;; Version: 1.0.1
 ;; Package-Requires:  ((popwin "0.4")(popup "0.5"))
 
-;;; Licence:
+;;; License:
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
