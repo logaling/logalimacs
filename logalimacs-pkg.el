aa95aca9f95451df279d5b7c384340d20bab7530
yuta yamada <yamada@clear-code.com>
2012-06-09 15:24:03 +0900

    temporary modify logalimacs-pkg.el to MELPA downloading test

--- logalimacs-pkg.el
+++ logalimacs-pkg.el
@@ -1,1 +1,1 @@
-(define-package "logalimacs" "1.0.0" "Front-end of logaling-command for Ruby gem" '((popwin "0.4")(popup "20120331")))
+(define-package "logalimacs" "1.0.0" "Front-end of logaling-command for Ruby gem" '((popwin "read from -pkg.el")(popup "20120331")))
