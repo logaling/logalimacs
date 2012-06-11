5355d6a94899559a9dcb1f51ad58dbfdb16a0923
yuta yamada <yamada@clear-code.com>
2012-06-11 09:07:35 +0900

    Fix a typo (")(" -> ") (")

--- logalimacs.el
+++ logalimacs.el
@@ -5,7 +5,7 @@
 ;; Author: Yuta Yamada <yamada@clear-code.com>
 ;; URL: https://github.com/logaling/logalimacs
 ;; Version: 1.0.0
-;; Package-Requires:  ((popwin "20120529")(popup "20120331"))
+;; Package-Requires:  ((popwin "20120529") (popup "20120331"))
 ;; Keywords: translation, lisp
 
 ;;; License:
