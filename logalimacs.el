ab18092b9d9d686ba1bcd840827af8443b01046b
Yuta Yamada <cokesboy@gmail.com>
2013-08-15 19:04:55 -0400

    Delete needless escape characters

--- logalimacs.el
+++ logalimacs.el
@@ -635,9 +635,9 @@ Because it escape character"
 (defun loga-japanese-p (word &optional choice)
   (lexical-let* ((striped-word (replace-regexp-in-string "'" "" word))
                  (sep      loga-mark-region-separator)
-                 (hiragana "\\p{hiragana}")
-                 (katakana "\\p{katakana}")
-                 (kanji    "\\p{Han}")
+                 (hiragana "\p{hiragana}")
+                 (katakana "\p{katakana}")
+                 (kanji    "\p{Han}")
                  (japanese-regexp
                   (case choice
                     (:hiragana hiragana)
